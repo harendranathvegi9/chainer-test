b9e40703a9664079fdd1defb7194e17d7802f9fd
Yuya Unno <unnonouno@gmail.com>
2017-03-16 14:59:56 +0900

    Install cupy every time

--- test.sh
+++ test.sh
@@ -1,8 +1,12 @@
 #!/bin/sh -ex
 
+cd cupy
+python setup.py build -j 4 develop install --user || python setup.py develop install --user
+cd ..
+
 cd chainer
 flake8
-python setup.py build -j 4 develop install --user || python setup.py develop install --user
+python setup.py install --user
 
 if [ $CUDNN = none ]; then
   nosetests --processes=4 --process-timeout=10000 --stop --with-coverage --cover-branches --cover-package=chainer -a '!cudnn,!slow'
